20da9be824c06f90fbf2b0a7c54c6a632369bdec
maycon douglas <maycondouglasbm10@gmail.com>
2026-04-20 22:08:34 -0300

    primeiros exercicios

---
+++ exercicios/exe02.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome: ")
+nome = nome.strip()  # Remove espaços em branco no início e no final
+nome = nome.title()  # Converte a primeira letra de cada palavra para maiúscula
+print("Olá, ", nome, "! Seja bem-vindo(a) ao curso de Python!")
